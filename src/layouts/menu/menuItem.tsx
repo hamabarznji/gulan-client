3cf469b4b5750f104c3bf92fcc359f6d08e77748
hamabarznji <hama.aak1@gmail.com>
2023-06-23 19:10:06 +0300

    updated styles and added active class

--- src/layouts/menu/menuItem.tsx
+++ src/layouts/menu/menuItem.tsx
@@ -4,35 +4,49 @@ import ListItem, { ListItemProps } from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import PersonIcon from '@mui/icons-material/Person';
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonIcon from "@mui/icons-material/Person";
 import { useRouter } from "next/router";
-
+import COLORS from "../../../public/COLORS";
 const menuItems: { title: string; icon: React.FC; path: string }[] = [
   { title: "Dashboard", icon: DashboardIcon, path: "/" },
   { title: "Profile", icon: PersonIcon, path: "/profile" },
   { title: "Users", icon: PersonIcon, path: "/users" },
-
 ];
 
 interface ListComponentProps {
   open: boolean;
 }
-
-const ListComponent: React.FC<ListComponentProps> = ({ open }: ListComponentProps) => {
+const activePath = (current: string, path: string) =>
+  current == path ? COLORS.primary : COLORS.gray;
+const ListComponent: React.FC<ListComponentProps> = ({
+  open,
+}: ListComponentProps) => {
   const router = useRouter();
+  const currentPath = router.pathname;
   return (
     <List>
       {menuItems.map((item, index) => (
-        <ListItem key={index} disablePadding sx={{ display: "block" }}
-        
-          onClick={()=>router.push(item.path)}
+        <ListItem
+          key={item?.path}
+          disablePadding
+          sx={{
+            display: "block",
+            color: activePath(currentPath, item?.path),
+            "&:hover": {
+              color: COLORS.primary,
+            },
+          }}
+          onClick={() => router.push(item?.path)}
         >
           <ListItemButton
             sx={{
               minHeight: 48,
               justifyContent: open ? "initial" : "center",
               px: 2.5,
+              onHover: {
+                backgroundColor: "rgba(0, 0, 0, 0.04)",
+              },
             }}
           >
             <ListItemIcon
@@ -40,6 +54,7 @@ const ListComponent: React.FC<ListComponentProps> = ({ open }: ListComponentProp
                 minWidth: 0,
                 mr: open ? 3 : "auto",
                 justifyContent: "center",
+                color: activePath(currentPath, item?.path),
               }}
             >
               <item.icon />
