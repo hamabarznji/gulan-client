20a58f334047c052e45aa4b1a1a99cb131acb5df
hamabarznji <hama.aak1@gmail.com>
2023-06-23 22:12:22 +0300

    updated

--- src/components/customComponents/Breadcrumb.tsx
+++ src/components/customComponents/Breadcrumb.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import Breadcrumbs from '@mui/material/Breadcrumbs';
-import Link from '@mui/material/Link';
-import HomeIcon from '@mui/icons-material/Home';
-import WhatshotIcon from '@mui/icons-material/Whatshot';
-import GrainIcon from '@mui/icons-material/Grain';
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
+import HomeIcon from "@mui/icons-material/Home";
+import WhatshotIcon from "@mui/icons-material/Whatshot";
+import GrainIcon from "@mui/icons-material/Grain";
 import menuItems from "../../layouts/menuItems";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 function handleClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
   event.preventDefault();
-  console.info('You clicked a breadcrumb.');
+  console.info("You clicked a breadcrumb.");
 }
 
 export default function IconBreadcrumbs() {
@@ -32,21 +32,26 @@ export default function IconBreadcrumbs() {
 
   const onClickHandler = (path: string) => {
     router.push(`/${path}`);
-  }
+  };
   return (
     <div role="presentation" onClick={handleClick}>
       <Breadcrumbs aria-label="breadcrumb" style={{ marginBottom: "1rem" }}>
         {pathsArray.map((item, index) => {
+          const isLastIndex = index === pathsArray.length - 1;
 
           const icon = getIconForPath(item);
           return (
             <Link
               key={item}
-              underline={index==pathsArray.length-1 ? "none" : "hover"}
-              sx={{ display: 'flex', alignItems: 'center' }}
-              color={index==0? 'inherit' : 'black'}
+              underline={index == pathsArray.length - 1 ? "none" : "hover"}
+              sx={{ display: "flex", alignItems: "center" }}
+              color={index == 0 ? "inherit" : "black"}
               href={item}
-              onClick={() => onClickHandler(item)}
+              onClick={() => {
+                if (!isLastIndex) {
+                  onClickHandler(item);
+                }
+              }}
             >
               {icon && (
                 <Typography variant="h5" component="span" sx={{ mr: 0.5 }}>
@@ -55,8 +60,7 @@ export default function IconBreadcrumbs() {
               )}
 
               <Typography variant="h5" component="span">
-               
-               {index==0&&'/'}
+                {index == 0 && "/"}
                 {item}
               </Typography>
             </Link>
