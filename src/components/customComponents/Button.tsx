db56ca97c80b0e3fdaefbd4db7c493a871a4d901
hamabarznji <hama.aak1@gmail.com>
2023-06-23 21:43:19 +0300

    added cutome button

---
+++ src/components/customComponents/Button.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button, ButtonProps, Typography } from '@mui/material';
+import COLORS from '../../../public/COLORS';
+import { Icon } from '@mui/material';
+
+const styles = {
+  backgroundColor: COLORS.primary,
+  color: '#fff',
+  '&:hover': {
+    opacity: 0.8,
+  },
+  display: 'flex',
+  alignItems: 'center',
+};
+
+interface CustomButtonProps extends ButtonProps {
+  title: string;
+  icon?: React.ReactNode;
+}
+
+const CustomButton: React.FC<CustomButtonProps> = (props) => {
+  const { title, icon, ...rest } = props;
+
+  return (
+    <Button {...rest} style={styles} startIcon={icon ? <Icon>{icon}</Icon> : null}>
+      <Typography>{title}</Typography>
+    </Button>
+  );
+};
+
+export default CustomButton;
