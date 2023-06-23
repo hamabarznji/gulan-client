bdaba2abdbeff5fb11e118f1c905302a07feee23
hamabarznji <hama.aak1@gmail.com>
2023-06-23 21:43:44 +0300

    seperated menu items

--- src/layouts/menu/menuItem.tsx
+++ src/layouts/menu/menuItem.tsx
@@ -9,12 +9,7 @@ import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
 import PersonIcon from "@mui/icons-material/Person";
 import { useRouter } from "next/router";
 import COLORS from "../../../public/COLORS";
-const menuItems: { title: string; icon: React.FC; path: string }[] = [
-  { title: "Dashboard", icon: DashboardIcon, path: "/" },
-  { title: "Invoices", icon: ReceiptLongIcon, path: "/invoices" },
-  { title: "Users", icon: PersonIcon, path: "/users" },
-];
-
+import menuItems from "../menuItems";
 interface ListComponentProps {
   open: boolean;
 }
