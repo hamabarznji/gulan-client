01dbea56446f3393b5bd6ba94946a8429a7efac4
hamabarznji <hama.aak1@gmail.com>
2023-06-23 21:44:18 +0300

    added breadcrumb

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import PersistentDrawerLeft from "./drawer";
-
+import Breadcrumb from "../components/customComponents/Breadcrumb";
 interface LayoutProps {
   children: React.ReactNode;
   toggleTheme: () => void;
@@ -19,17 +19,19 @@ const Layout: React.FC<LayoutProps> = ({
       ? "Home"
       : router?.asPath?.split("/")[1].charAt(0).toUpperCase() +
         router?.asPath?.split("/")[1].slice(1);
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{title}</title>
       </Head>
-      <main>
+      <main style={{ padding: "2rem", backgroundColor: "#f5f5f5" }}>
         {router.pathname === "/login" ? (
           children
         ) : (
           <PersistentDrawerLeft toggleTheme={toggleTheme} themeMode={themeMode}>
+            <Breadcrumb/>
             {children}
           </PersistentDrawerLeft>
         )}
