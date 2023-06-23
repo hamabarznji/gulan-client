6dfd0e888e263cb0e5f57760f99d2ca768bb95ad
hamabarznji <hama.aak1@gmail.com>
2023-06-23 23:47:59 +0300

    invoice header

---
+++ src/components/invoice/add/Header.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Typography, Grid } from "@mui/material";
+import Image from "next/image";
+import icon from "../../../../public/icon.png";
+interface HeaderProps {
+  date: string;
+  id: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ id, date }) => {
+  return (
+    <>
+      <Grid item xs={12} sm={6}>
+        <Image src={icon} alt="Shopping Cart Icon" width={150} height={150} />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Typography variant="h3" align="right" gutterBottom color={"black"}>
+          INVOICE
+        </Typography>
+      </Grid>
+
+      <Grid item xs={12} sm={6}>
+        <Typography variant="h6" align="left" color="black">
+          GUL GUL Shoping
+        </Typography>
+        <Typography variant="h6" align="left" color="black">
+          Erbil Gullan Mall
+        </Typography>
+        <Typography variant="h6" align="left" color="black">
+          +964 750 000 0000
+        </Typography>
+      </Grid>
+
+      <Grid item xs={12} sm={6}>
+        <Typography variant="h6" align="right" color="black">
+          Invoice No: {id}
+        </Typography>
+        <Typography variant="h6" align="right" color="black">
+          Date: {date}
+        </Typography>
+        <Typography variant="h6" align="right" color="black">
+          Issued By: MB
+        </Typography>
+      </Grid>
+
+    </>
+  );
+};
+
+export default Header;
