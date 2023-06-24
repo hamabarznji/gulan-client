2da94299b7e5a75bfef0addb460f8167a9b985cd
hamabarznji <hama.aak1@gmail.com>
2023-06-24 23:57:34 +0300

    added users page

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,11 +1,19 @@
-import * as React from 'react';
-import UsersComponent from '../src/components/user/users'
-
-
+import * as React from "react";
+import UsersTable from "../src/components/user/UsersTable";
+import { Grid } from "@mui/material";
+import Button from "../src/components/customComponents/Button";
+import Modal from "../src/components/customComponents/Modal";
+import AddIcon from "@mui/icons-material/Add";
 export default function Users() {
-
-
   return (
-    <UsersComponent/>
+    <Grid container justifyContent="flex-start" spacing={3} direction="row">
+      <Grid item container justifyContent="flex-start">
+        <Modal processTitle="Add New User" modalTitle="Add New User" />
+      </Grid>
+
+      <Grid item container direction="row" justifyContent="center">
+        <UsersTable />
+      </Grid>
+    </Grid>
   );
 }
