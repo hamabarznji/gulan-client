447e424235ccb223da7ef7d2d4b8b73d36b8425f
hamabarznji <hama.aak1@gmail.com>
2023-06-24 22:12:06 +0300

    improved custome button

--- src/components/customComponents/Button.tsx
+++ src/components/customComponents/Button.tsx
@@ -1,31 +1,36 @@
-import React from 'react';
-import { Button, ButtonProps, Typography } from '@mui/material';
-import COLORS from '../../../public/COLORS';
-import { Icon } from '@mui/material';
+import React from "react";
+import { Button, ButtonProps, Typography, Icon } from "@mui/material";
+import COLORS from "../../../public/COLORS";
 
+const CustomButton: React.FC<CustomButtonProps> = ({ title, icon, ...rest }) => {
 const styles = {
-  backgroundColor: COLORS.primary,
-  color: '#fff',
-  '&:hover': {
-    opacity: 0.8,
-  },
-  display: 'flex',
-  alignItems: 'center',
+backgroundColor: COLORS.primary,
+color: "#fff",
+"&:hover": {
+opacity: 0.8,
+},
+display: "flex",
+alignItems: "center",
+};
+
+const iconStyles = {
+fontSize: "large",
+display: "flex",
+justifyContent: "center",
+alignItems: "center",
+};
+
+return (
+<Button {...rest} style={styles}>
+{icon && <Icon style={iconStyles}>{icon}</Icon>}
+<Typography>{title}</Typography>
+</Button>
+);
 };
 
 interface CustomButtonProps extends ButtonProps {
-  title?: string;
-  icon?: React.ReactNode;
+title: string;
+icon?: React.ReactNode;
 }
-
-const CustomButton: React.FC<CustomButtonProps> = (props) => {
-  const { title, icon, ...rest } = props;
-
-  return (
-    <Button {...rest} style={styles} startIcon={icon ? <Icon>{icon}</Icon> : null}>
-     {title&& <Typography>{title}</Typography>}
-    </Button>
-  );
-};
 
 export default CustomButton;
