d2c9efa13c1a9e3246bd60957da2e5fb45cf35fa
hamabarznji <hama.aak1@gmail.com>
2023-06-24 22:01:56 +0300

    cleaned page

--- src/components/user/usersTable.tsx
+++ src/components/user/usersTable.tsx
@@ -1,82 +1,41 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { blue } from '@mui/material/colors';
-import { useQuery,QueryKey } from '@tanstack/react-query'
-import UserServiceInstance from '../../../services/user';
+import * as React from "react";
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: blue[500],
-    color: theme.palette.common.white,
-    fontWeight: 'bold',
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
+import { useQuery, QueryKey } from "@tanstack/react-query";
+import UserServiceInstance from "../../../services/user";
+import UsersTable from "../customComponents/Table";
 
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
 
-function createData(username: string, role: string) {
-  return { username, role };
-}
 
-const rows = [
-  
-  createData('Hama', 'admin'),
-  createData('Gulan', 'admin'),
-  createData('Sara', 'user'),
+const columns:any[] = [
+  { id: "id", label: "ID", align: "center" },
+  { id: "name", label: "Name", align: "center" },
+  { id: "role", label: "Role", align: "center" },
 ];
-const queryKey: QueryKey = ['users']; 
+
+const rows:any[] = [
+  {
+    id: 1,
+    name: "John Doe",
+    role: "Admin",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    role: "User",
+  },
+];
+const queryKey: QueryKey = ["users"];
 
 export default function CustomizedTables() {
-
   const fetchUsers = async () => {
     try {
-     return await UserServiceInstance.getUsers();
+      return await UserServiceInstance.getUsers();
     } catch (error) {
-      throw new Error('Failed to fetch users');
+      throw new Error("Failed to fetch users");
     }
   };
-  
-    const {  data:users } = useQuery(queryKey, fetchUsers);
-  //     const rows = users?.map((user: any) => createData(user.username, user.role));
 
-  console.log(users)
-  return (
-    <TableContainer component={Paper} sx={{ width: '100%' }}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell align="right">Username</StyledTableCell>
-            <StyledTableCell align="right">Role</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.username}>
-              <StyledTableCell component="th" scope="row">
-                {row.username}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.role}</StyledTableCell>
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+  const { data: users } = useQuery(queryKey, fetchUsers);
+
+  return <UsersTable rows={rows} columns={columns} />;
 }
