fe78b5d1df6b837b4eac43a5fd8fe278b6996c59
hamabarznji <hama.aak1@gmail.com>
2023-06-24 22:45:27 +0300

    added products

--- src/layouts/menuItems.tsx
+++ src/layouts/menuItems.tsx
@@ -2,10 +2,11 @@
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
 import PersonIcon from "@mui/icons-material/Person";
-import { useRouter } from "next/router";
+import InventoryIcon from '@mui/icons-material/Inventory';
 const menuItems: { title: string; icon: React.FC; path: string }[] = [
     { title: "Dashboard", icon: DashboardIcon, path: "/dashboard" },
     { title: "Invoices", icon: ReceiptLongIcon, path: "/invoices" },
+    { title: "Products", icon: InventoryIcon, path: "/products" },
     { title: "Users", icon: PersonIcon, path: "/users" },
   ];
 
