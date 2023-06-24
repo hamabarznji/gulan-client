bee9dae346c3f65db698e5e7c089b046d033ba3d
hamabarznji <hama.aak1@gmail.com>
2023-06-24 22:45:43 +0300

    removed extra lines

--- src/components/invoice/InvoicesTable.tsx
+++ src/components/invoice/InvoicesTable.tsx
@@ -1,36 +1,29 @@
-import React from 'react';
-import Table from '../../components/customComponents/Table';
-
-
-
+import React from "react";
+import Table from "../../components/customComponents/Table";
 
 const rows = Array.from(Array(50), (_, index) => ({
-    id: index + 1,
-    name: `Person ${index + 1}`,
-    age: Math.floor(Math.random() * 50) + 20,
-    email: `person${index + 1}@example.com`,
-  }));
+  id: index + 1,
+  name: `Person ${index + 1}`,
+  age: Math.floor(Math.random() * 50) + 20,
+  email: `person${index + 1}@example.com`,
+}));
 
-  const columns = [
-    { id: 'id', label: 'ID', align: 'center' },
-    { id: 'name', label: 'Name', align: 'center' },
-    { id: 'age', label: 'Age', align: 'center' },
-    { id: 'email', label: 'Email', align: 'center' },
-    { id: 'actions', label: 'Action', align: 'center' },
-  ];
+const columns = [
+  { id: "id", label: "ID", align: "center" },
+  { id: "name", label: "Name", align: "center" },
+  { id: "age", label: "Age", align: "center" },
+  { id: "email", label: "Email", align: "center" },
+  { id: "actions", label: "Action", align: "center" },
+];
 
 const InvoiceTable: React.FC = () => {
   // Your component logic here
 
   return (
     <>
-      <Table 
-        rows={rows}
-        columns={columns}
-      
-      />
+      <Table rows={rows} columns={columns} />
     </>
   );
-}
+};
 
 export default InvoiceTable;
