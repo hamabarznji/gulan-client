9f1528d95949164713f41c5c8aa3efda62f8ba0f
hamabarznji <hama.aak1@gmail.com>
2023-06-25 18:47:44 +0300

    added 'adduser' component

--- pages/users.tsx
+++ pages/users.tsx
@@ -4,11 +4,13 @@ import { Grid } from "@mui/material";
 import Button from "../src/components/customComponents/Button";
 import Modal from "../src/components/customComponents/Modal";
 import AddIcon from "@mui/icons-material/Add";
+import AddUser from "../src/components/user/AddUser";
+
 export default function Users() {
   return (
     <Grid container justifyContent="flex-start" spacing={3} direction="row">
       <Grid item container justifyContent="flex-start">
-        <Modal processTitle="Add New User" modalTitle="Add New User" />
+        <AddUser />
       </Grid>
 
       <Grid item container direction="row" justifyContent="center">
