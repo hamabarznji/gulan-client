a6e27e329f867881cf9074a742b4877954fb56f7
hamabarznji <hama.aak1@gmail.com>
2023-06-25 17:11:10 +0300

    fixed key issue

--- src/components/customComponents/Table.tsx
+++ src/components/customComponents/Table.tsx
@@ -1,14 +1,5 @@
 import React, { ChangeEvent } from "react";
-import {
-  Table,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  Paper,
-  TablePagination,
-} from "@mui/material";
+import { Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Paper, TablePagination } from '@mui/material';
 
 interface TableProps {
   rows: any[];
@@ -19,15 +10,12 @@ interface TableProps {
 const CustomeTable: React.FC<TableProps> = ({
   rows,
   columns,
-  isPagination = true,
+  isPagination = true
 }: TableProps) => {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-  const handleChangePage = (
-    event: React.MouseEvent<HTMLButtonElement> | null,
-    newPage: number
-  ) => {
+  const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
     event?.preventDefault();
     setPage(newPage);
   };
@@ -38,25 +26,23 @@ const CustomeTable: React.FC<TableProps> = ({
   };
 
   return (
-    <Paper sx={{ width: "100%", overflow: "hidden" }}>
-      <TableContainer
-        sx={{
-          maxHeight: "calc(100vh - 200px)",
-          "@media (max-width: 600px)": {
-            maxHeight: "calc(100vh - 300px)",
-          },
-        }}
-      >
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{
+        maxHeight: 'calc(100vh - 200px)',
+        '@media (max-width: 600px)': {
+          maxHeight: 'calc(100vh - 300px)',
+        },
+      }}>
         <Table>
           <TableHead
             sx={{
-              backgroundColor: "#d3d3d3",
-              color: "#ff7f50",
-              fontWeight: "bold",
-              fontSize: "1.2rem",
-              "& th": {
-                color: "black",
-                fontWeight: "bold",
+              backgroundColor: '#d3d3d3',
+              color: '#ff7f50',
+              fontWeight: 'bold',
+              fontSize: '1.2rem',
+              '& th': {
+                color: 'black',
+                fontWeight: 'bold',
               },
             }}
           >
@@ -65,7 +51,7 @@ const CustomeTable: React.FC<TableProps> = ({
                 <TableCell
                   key={column.id}
                   align={column.align}
-                  sx={{ fontSize: "1rem", padding: "8px" }}
+                  sx={{ fontSize: '1rem', padding: '8px' }}
                 >
                   {column.label}
                 </TableCell>
@@ -77,18 +63,12 @@ const CustomeTable: React.FC<TableProps> = ({
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((row) => {
                 return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
                     {columns.map((column) => {
                       const value = row[column.id];
                       return (
-                        <TableCell
-                          key={row.id}
-                          align="center"
-                          sx={{ fontSize: "1rem", color: "black" }}
-                        >
-                          {column.format && typeof value === "number"
-                            ? column.format(value)
-                            : value}
+                        <TableCell key={column.id} align="center" sx={{ fontSize: "1rem", color: "black" }}>
+                          {column.format && typeof value === 'number' ? column.format(value) : value}
                         </TableCell>
                       );
                     })}
@@ -98,17 +78,15 @@ const CustomeTable: React.FC<TableProps> = ({
           </TableBody>
         </Table>
       </TableContainer>
-      {isPagination && (
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 100]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      )}
+      {isPagination && <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />}
     </Paper>
   );
 };
