5dd955ccdb3502d74fab161a742e33750edb8f87
hamabarznji <hama.aak1@gmail.com>
2023-06-26 23:25:23 +0300

    added isCurrentUserAllowed

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
 import UsersTable from "../src/components/user/UsersTable";
+import isCurrentUserAllowed from "../src/utils/Redirect";
+
+export const getServerSideProps = isCurrentUserAllowed;
 
 const Users = () => {
   return <UsersTable />;
