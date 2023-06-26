97dfa84f9451e5abcd445fdcc2f2127703bfed17
hamabarznji <hama.aak1@gmail.com>
2023-06-26 22:17:35 +0300

    render pages condetionally

--- src/layouts/menu/menuItem.tsx
+++ src/layouts/menu/menuItem.tsx
@@ -1,64 +1,70 @@
-import React from "react";
+import React, { useContext } from "react";
 import List from "@mui/material/List";
-import ListItem, { ListItemProps } from "@mui/material/ListItem";
+import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
-import PersonIcon from "@mui/icons-material/Person";
+import { UserContext } from "../../context/UserContext";
+
 import { useRouter } from "next/router";
 import COLORS from "../../../public/COLORS";
 import menuItems from "../menuItems";
+
 interface ListComponentProps {
   open: boolean;
 }
+
 const activePath = (current: string, path: string) =>
   current == path ? COLORS.primary : COLORS.gray;
-const ListComponent: React.FC<ListComponentProps> = ({
-  open,
-}: ListComponentProps) => {
+
+const ListComponent: React.FC<ListComponentProps> = ({ open }: ListComponentProps) => {
+  const { user } = useContext(UserContext);
+  const userRole = user?.role;
+console.log(userRole)
   const router = useRouter();
   const currentPath = router.pathname;
+
   return (
     <List>
-      {menuItems.map((item, index) => (
-        <ListItem
-          key={item?.path}
-          disablePadding
-          sx={{
-            display: "block",
-            color: activePath(currentPath, item?.path),
-            "&:hover": {
-              color: COLORS.primary,
-            },
-          }}
-          onClick={() => router.push(item?.path)}
-        >
-          <ListItemButton
+      {menuItems.map((item) =>
+        item.roles.includes(userRole) ? (
+          <ListItem
+            key={item?.path}
+            disablePadding
             sx={{
-              minHeight: 48,
-              justifyContent: open ? "initial" : "center",
-              px: 2.5,
-              onHover: {
-                backgroundColor: "rgba(0, 0, 0, 0.04)",
+              display: "block",
+              color: activePath(currentPath, item?.path),
+              "&:hover": {
+                color: COLORS.primary,
               },
             }}
+            onClick={() => router.push(item?.path)}
           >
-            <ListItemIcon
+            <ListItemButton
               sx={{
-                minWidth: 0,
-                mr: open ? 3 : "auto",
-                justifyContent: "center",
-                color: activePath(currentPath, item?.path),
+                minHeight: 48,
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+                onHover: {
+                  backgroundColor: "rgba(0, 0, 0, 0.04)",
+                },
               }}
             >
-              <item.icon />
-            </ListItemIcon>
-            <ListItemText primary={item.title} sx={{ opacity: open ? 1 : 0 }} />
-          </ListItemButton>
-        </ListItem>
-      ))}
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                  color: activePath(currentPath, item?.path),
+                }}
+              >
+                <item.icon />
+              </ListItemIcon>
+              <ListItemText primary={item.title} sx={{ opacity: open ? 1 : 0 }} />
+            </ListItemButton>
+          </ListItem>
+        ) : null
+      )}
     </List>
   );
 };
