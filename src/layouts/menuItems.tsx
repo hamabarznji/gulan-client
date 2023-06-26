4cbac60e1aff7277b991fc558f991acced52f00e
hamabarznji <hama.aak1@gmail.com>
2023-06-26 22:17:12 +0300

    added roles

--- src/layouts/menuItems.tsx
+++ src/layouts/menuItems.tsx
@@ -3,11 +3,11 @@ import DashboardIcon from "@mui/icons-material/Dashboard";
 import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
 import PersonIcon from "@mui/icons-material/Person";
 import InventoryIcon from '@mui/icons-material/Inventory';
-const menuItems: { title: string; icon: React.FC; path: string }[] = [
-    { title: "Dashboard", icon: DashboardIcon, path: "/dashboard" },
-    { title: "Invoices", icon: ReceiptLongIcon, path: "/invoices" },
-    { title: "Products", icon: InventoryIcon, path: "/products" },
-    { title: "Users", icon: PersonIcon, path: "/users" },
+const menuItems: { title: string; icon: React.FC; path: string ,roles:any[]}[] = [
+    { title: "Dashboard", icon: DashboardIcon, path: "/dashboard",roles:['admin'] },
+    { title: "Invoices", icon: ReceiptLongIcon, path: "/invoices" ,roles:['admin','user']},
+    { title: "Products", icon: InventoryIcon, path: "/products",roles:['admin'] },
+    { title: "Users", icon: PersonIcon, path: "/users",roles:['admin'] },
   ];
 
   export default menuItems;
