233fd4fcd56485ab48a83917920c34d7b56e4c21
hamabarznji <hama.aak1@gmail.com>
2023-06-26 22:32:41 +0300

    created user context for storing user data

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,50 @@
+import React, { createContext, useEffect, useState } from "react";
+import UserServiceInstance from "../../services/user";
+
+interface UserData {
+  id: string;
+  username: string;
+  role: string;
+}
+
+interface UserContextProps {
+  user: UserData | null;
+  clearContext: () => void;
+  fetchUserData: () => void;
+}
+
+interface UserProviderProps {
+  children: React.ReactNode;
+}
+
+export const UserContext = createContext<UserContextProps>({
+  user: null,
+  clearContext: () => {},
+  fetchUserData: () => {},
+});
+
+export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<UserData | null>(null);
+  const fetchUserData = async () => {
+    try {
+      const response: UserData = await UserServiceInstance.getSession();
+      setUser(response);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+  useEffect(() => {
+    console.log("UserProvider mounted");
+   
+
+    fetchUserData();
+  }, []);
+
+  const clearContext = () => {
+    setUser(null);
+  };
+
+  return (
+    <UserContext.Provider value={{ user,clearContext,fetchUserData }}>{children}</UserContext.Provider>
+  );
+};
