f131eed99af63c626d7b9eb131dfb5ca598e24f7
hamabarznji <hama.aak1@gmail.com>
2023-06-26 19:18:32 +0300

    added data to add interface

--- src/interfaces/user/add.tsx
+++ src/interfaces/user/add.tsx
@@ -1,31 +1,45 @@
 import InputsFieldInterface from "../InputsFieldInterface";
 
 const addUserInputs: InputsFieldInterface[] = [
-    {
-      id: 1,
-      label: "User Name",
-      name: "username",
-      type: "string",
-      required: true,
-      helperText: "Username is required",
-    },
-    {
-      id: 2,
-      label: "Password",
-      name: "password",
-      type: "string",
-      required: true,
-      helperText: "Password is required",
-    },
-    {
-      id: 3,
-      label: "Role",
-      name: "role",
-      type: "string",
-      required: true,
-      helperText: "Role is required",
-    },
-  ];
-  
+  {
+    id: 1,
+    label: "User Name",
+    name: "username",
+    type: "string",
+    required: true,
+    helperText: "Username is required",
+    isMenu: false,
+  },
+  {
+    id: 2,
+    label: "Password",
+    name: "password",
+    type: "string",
+    required: true,
+    helperText: "Password is required",
+    isMenu: false,
+  },
+  {
+    id: 3,
+    label: "Role",
+    name: "role",
+    type: "string",
+    required: true,
+    helperText: "Role is required",
+    isMenu: true,
+    options: [
+      {
+        id: 1,
+        label: "Admin",
+        value: "admin",
+      },
+      {
+        id: 2,
+        label: "User",
+        value: "user",
+      },
+    ],
+  },
+];
 
-  export default addUserInputs;
+export default addUserInputs;
