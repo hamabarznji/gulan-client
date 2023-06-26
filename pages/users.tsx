62e65b895d2f4a6213c0c8e8d817dbcdc1009e58
hamabarznji <hama.aak1@gmail.com>
2023-06-26 20:23:09 +0300

    removed extra lines

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import UsersTable from "../src/components/user/UsersTable";
 import { Grid } from "@mui/material";
-import Button from "../src/components/customComponents/Button";
-import Modal from "../src/components/customComponents/Modal";
-import AddIcon from "@mui/icons-material/Add";
 import AddUser from "../src/components/user/AddUser";
 
 export default function Users() {
