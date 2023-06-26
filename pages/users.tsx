b1d461e74566199122a6796fa6d6f8ae98b36067
hamabarznji <hama.aak1@gmail.com>
2023-06-26 20:52:48 +0300

    fixed reload error

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,18 +1,7 @@
 import * as React from "react";
 import UsersTable from "../src/components/user/UsersTable";
-import { Grid } from "@mui/material";
-import AddUser from "../src/components/user/AddUser";
 
-export default function Users() {
-  return (
-    <Grid container justifyContent="flex-start" spacing={3} direction="row">
-      <Grid item container justifyContent="flex-start">
-        <AddUser />
-      </Grid>
-
-      <Grid item container direction="row" justifyContent="center">
-        <UsersTable />
-      </Grid>
-    </Grid>
-  );
-}
+const Users = () => {
+  return <UsersTable />;
+};
+export default Users;
