8673433a33860dc1e9a268d85043cd400b19960c
hamabarznji <hama.aak1@gmail.com>
2023-06-26 20:53:30 +0300

    refetch after add new user

--- src/components/user/AddUser.tsx
+++ src/components/user/AddUser.tsx
@@ -2,18 +2,23 @@ import React from "react";
 import UserServiceInstance from "../../../services/user";
 import { useSnackbar } from "notistack";
 import InputFields from "../customComponents/InputFieldsWithValidation";
-
 import addUserInputs from "../../interfaces/user/add";
 
-const AddUser: React.FC = () => {
+const AddUser: React.FC = ({reFetchUsers}:any) => {
   const { enqueueSnackbar } = useSnackbar();
 
   const submitHandler = async (data: any) => {
 
     try {
-      const newUser: any = await UserServiceInstance.addUser(data);
+      const newUser: any = await UserServiceInstance.addUser({
+        username: data.name,
+        role: data.role,
+        password: data.password,
+
+      });
 
       if (newUser.status === 200) {
+        reFetchUsers();
         enqueueSnackbar("New User Added Successfully!", {
           variant: "success",
         });
