c22b81bd6adaf3e762eb0923f76141162071a520
hamabarznji <hama.aak1@gmail.com>
2023-06-27 04:28:27 +0300

    added new path

--- src/interfaces/menuItems.tsx
+++ src/interfaces/menuItems.tsx
@@ -1,13 +1,22 @@
-
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
 import PersonIcon from "@mui/icons-material/Person";
-import InventoryIcon from '@mui/icons-material/Inventory';
-const menuItems: { title: string; icon: React.FC; path: string ,roles:any[]}[] = [
-    { title: "Dashboard", icon: DashboardIcon, path: "/dashboard",roles:['admin'] },
-    { title: "Invoices", icon: ReceiptLongIcon, path: "/invoices" ,roles:['admin','user']},
-    { title: "Products", icon: InventoryIcon, path: "/products",roles:['admin'] },
-    { title: "Users", icon: PersonIcon, path: "/users",roles:['admin'] },
-  ];
+import InventoryIcon from "@mui/icons-material/Inventory";
+import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
+interface MenuItem {
+  title: string;
+  icon: React.ComponentType;
+  path: string;
+  roles: string[];
+  isImage?: boolean;
+}
 
-  export default menuItems;
+const menuItems: MenuItem[] = [
+  { title: "Dashboard", icon: DashboardIcon, path: "/dashboard", roles: ["admin"],isImage: false },
+  { title: "Invoices", icon: ReceiptLongIcon, path: "/invoices", roles: ["admin", "user"],isImage: false },
+  { title: "Products", icon: InventoryIcon, path: "/products", roles: ["admin"],isImage: false },
+  { title: "Users", icon: PersonIcon, path: "/users", roles: ["admin"] },
+  { title: "Expenses", icon: PointOfSaleIcon, path: "/expenses", roles: ["admin"],isImage: true },
+];
+
+export default menuItems;
