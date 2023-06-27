2c13ebc32a70cdd9d5c4ccda91e375dea08242c7
hamabarznji <hama.aak1@gmail.com>
2023-06-27 17:49:26 +0300

    make the button style be override

--- src/components/customComponents/Button.tsx
+++ src/components/customComponents/Button.tsx
@@ -2,35 +2,43 @@ import React from "react";
 import { Button, ButtonProps, Typography, Icon } from "@mui/material";
 import COLORS from "../../../public/COLORS";
 
-const CustomButton: React.FC<CustomButtonProps> = ({ title, icon, ...rest }) => {
-const styles = {
-backgroundColor: COLORS.primary,
-color: "#fff",
-"&:hover": {
-opacity: 0.8,
-},
-display: "flex",
-alignItems: "center",
-};
+const CustomButton: React.FC<CustomButtonProps> = ({
+  title,
+  icon,
+  isEdit=false,
 
-const iconStyles = {
-fontSize: "large",
-display: "flex",
-justifyContent: "center",
-alignItems: "center",
-};
+  ...rest
+}) => {
+  const styles = {
+    backgroundColor:isEdit?"#934eb0": COLORS.primary,
+    color: "#fff",
+    "&:hover": {
+      opacity: 0.8,
+    },
+    display: "flex",
+    alignItems: "center",
+    ...rest.style,
+  };
 
-return (
-<Button {...rest} style={styles}>
-{icon && <Icon style={iconStyles}>{icon}</Icon>}
-<Typography>{title}</Typography>
-</Button>
-);
+  const iconStyles = {
+    fontSize: "large",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  return (
+    <Button {...rest} style={styles}>
+      {icon && <Icon style={iconStyles}>{icon}</Icon>}
+      <Typography>{title}</Typography>
+    </Button>
+  );
 };
 
 interface CustomButtonProps extends ButtonProps {
-title?: string;
-icon?: React.ReactNode;
+  title?: string;
+  icon?: React.ReactNode;
+  isEdit?: boolean;
 }
 
 export default CustomButton;
