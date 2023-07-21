cf675525c4a98b3250f3c2752866c40afa5d9a06
hamabarznji <hama.aak1@gmail.com>
2023-07-21 17:17:35 +0300

    added components

--- pages/expenses/index.tsx
+++ pages/expenses/index.tsx
@@ -1,10 +1,29 @@
 import * as React from "react";
 import ExpensesTable from "../../src/components/expense/ExpensesTable";
 import isCurrentUserAllowed from "../../src/utils/Redirect";
-
+import { Grid } from "@mui/material";
+import InvoiceCard from "../../src/components/invoice/InvoiceCard";
+import PieChartPage from "../../src/components/expense/Chart";
 export const getServerSideProps = isCurrentUserAllowed;
 
 const Expenses = () => {
-  return <ExpensesTable />;
+  return (
+    <>
+      <Grid container justifyContent="flex-start" spacing={2} direction="row">
+        <Grid
+          item
+          container
+          direction="row"
+          // spacing={theme.spacing(2)}
+          justifyContent="center"
+        >
+          <PieChartPage />
+        </Grid>
+        <Grid item container direction="row" justifyContent="center">
+          <ExpensesTable />
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 export default Expenses;
