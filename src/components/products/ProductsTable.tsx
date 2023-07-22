47b589222368795115acdeb545bc0446ee8a5b87
hamabarznji <hama.aak1@gmail.com>
2023-07-22 22:03:01 +0300

    added reall data

--- src/components/products/ProductsTable.tsx
+++ src/components/products/ProductsTable.tsx
@@ -1,45 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from '../../components/customComponents/Table';
-
-
-
-const rows = [
-  { id: 1, name: 'T-Shirt', qty: 5, color: 'Red', size: 'Medium' },
-  { id: 2, name: 'Jeans', qty: 10, color: 'Blue', size: 'Large' },
-  { id: 3, name: 'Dress', qty: 3, color: 'Green', size: 'Small' },
-  { id: 4, name: 'Hoodie', qty: 8, color: 'Black', size: 'Large' },
-  { id: 5, name: 'Skirt', qty: 2, color: 'Pink', size: 'Medium' },
-  { id: 6, name: 'Sweater', qty: 6, color: 'Gray', size: 'Small' },
-  { id: 7, name: 'Jacket', qty: 4, color: 'Brown', size: 'Large' },
-  { id: 8, name: 'Blouse', qty: 7, color: 'White', size: 'Small' },
-  { id: 9, name: 'Shorts', qty: 3, color: 'Yellow', size: 'Medium' },
-  { id: 10, name: 'Coat', qty: 9, color: 'Navy', size: 'Large' },
-  { id: 11, name: 'Pants', qty: 6, color: 'Beige', size: 'Medium' },
-  { id: 12, name: 'Sweatshirt', qty: 4, color: 'Purple', size: 'Small' },
-  { id: 13, name: 'Shirt', qty: 8, color: 'Orange', size: 'Large' },
-  { id: 14, name: 'Leggings', qty: 5, color: 'Black', size: 'Medium' },
-  { id: 15, name: 'Blazer', qty: 2, color: 'Gray', size: 'Small' },
-  { id: 16, name: 'Top', qty: 3, color: 'Pink', size: 'Medium' },
-  { id: 17, name: 'Sweatpants', qty: 6, color: 'Blue', size: 'Large' },
-  { id: 18, name: 'Cardigan', qty: 7, color: 'Green', size: 'Small' },
-  { id: 19, name: 'Dungarees', qty: 4, color: 'Denim', size: 'Medium' },
-  { id: 20, name: 'Jumpsuit', qty: 3, color: 'Black', size: 'Large' },
-  // Add more objects as needed
-];
-
-
+import ItemsServiceInstance from '../../../services/ItemService';
+import { useQuery, QueryKey } from "@tanstack/react-query";
+const queryKey: QueryKey = ["items"];
   const columns = [
-    { id: 'id', label: 'ID', align: 'center' },
+    { id: 'index', label: '#', align: 'center' },
     { id: 'name', label: 'Name', align: 'center' },
     { id: 'qty', label: 'Quantity', align: 'center' },
+    { id: 'price', label: 'Price', align: 'center' },
     { id: 'color', label: 'Color', align: 'center' },
     { id: 'size', label: 'Size', align: 'center' },
+    { id: 'categoryName', label: 'Category', align: 'center' },
     { id: 'actions', label: 'Action', align: 'center' },
   ];
 
 const InvoiceTable: React.FC = () => {
-  // Your component logic here
 
+  const fetchItems = async () => {
+    try {
+      const response = await ItemsServiceInstance.getItems();
+      return response
+    } catch (error) {
+      throw new Error("Failed to fetch users");
+    }
+  };
+  const { data, refetch } = useQuery(queryKey, fetchItems);
+const rows=data.map((item:any,index:any)=>{
+  return {
+    ...item,
+    index:index+1,
+    name:item.itemName,
+    price:item.price,
+    code:item.itemCode,
+    color:item.itemColor,
+    size:item.itemSize,
+    categoryName:item.categoryName,
+
+  }
+})
   return (
     <>
       <Table 
