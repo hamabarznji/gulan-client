ba3d601facfadae1d0b6bac6226053b3fd76c6e2
hamabarznji <hama.aak1@gmail.com>
2023-07-23 22:30:27 +0300

    added nested links

--- src/interfaces/menuItems.tsx
+++ src/interfaces/menuItems.tsx
@@ -2,21 +2,69 @@ import DashboardIcon from "@mui/icons-material/Dashboard";
 import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
 import PersonIcon from "@mui/icons-material/Person";
 import InventoryIcon from "@mui/icons-material/Inventory";
-import PointOfSaleIcon from '@mui/icons-material/PointOfSale';
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import CategoryIcon from '@mui/icons-material/Category';
+import BusinessIcon from '@mui/icons-material/Business';
+
 interface MenuItem {
   title: string;
   icon: React.ComponentType;
   path: string;
   roles: string[];
-  isImage?: boolean;
+  nestedLinks?: boolean;
+  nestedLinksItems?: [];
 }
 
 const menuItems: MenuItem[] = [
-  { title: "Dashboard", icon: DashboardIcon, path: "/dashboard", roles: ["admin"],isImage: false },
-  { title: "Invoices", icon: ReceiptLongIcon, path: "/invoices", roles: ["admin", "user"],isImage: false },
-  { title: "Items", icon: InventoryIcon, path: "/items", roles: ["admin"],isImage: false },
-  { title: "Users", icon: PersonIcon, path: "/users", roles: ["admin"] },
-  { title: "Expenses", icon: PointOfSaleIcon, path: "/expenses", roles: ["admin"],isImage: true },
+  {
+    title: "Dashboard",
+    icon: DashboardIcon,
+    path: "/dashboard",
+    roles: ["admin"],
+    nestedLinks: false,
+  },
+  {
+    title: "Invoices",
+    icon: ReceiptLongIcon,
+    path: "/invoices",
+    roles: ["admin", "user"],
+  },
+  {
+    title: "Items",
+    icon: InventoryIcon,
+    path: "/items",
+    roles: ["admin"],
+    nestedLinks: true,
+    nestedLinksItems: [
+      {
+        title: "Categories",
+        icon: CategoryIcon,
+        path: "/items/category",
+        roles: ["admin"],
+        isImage: false,
+      },
+      {
+        title: "Vendors",
+        icon: BusinessIcon,
+        path: "/items/vendors",
+        roles: ["admin"],
+      },
+    ],
+  },
+  {
+    title: "Users",
+    icon: PersonIcon,
+    path: "/users",
+    nestedLinks: false,
+    roles: ["admin"],
+  },
+  {
+    title: "Expenses",
+    icon: PointOfSaleIcon,
+    path: "/expenses",
+    nestedLinks: false,
+    roles: ["admin"],
+  },
 ];
 
 export default menuItems;
--- src/layouts/menu/menuItem.tsx
+++ src/layouts/menu/menuItem.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import * as React from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import useUserContext from "../../context/useUserContext";
+import Collapse from "@mui/material/Collapse";
+import ListSubheader from "@mui/material/ListSubheader";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
 
+import useUserContext from "../../context/useUserContext";
 import { useRouter } from "next/router";
 import COLORS from "../../../public/COLORS";
 import menuItems from "../../interfaces/menuItems";
@@ -15,7 +19,7 @@ interface ListComponentProps {
 }
 
 const activePath = (current: string, path: string) =>
-  current == path ? COLORS.primary : COLORS.gray;
+  current === path ? COLORS.primary : COLORS.gray;
 
 const ListComponent: React.FC<ListComponentProps> = ({
   open,
@@ -25,49 +29,129 @@ const ListComponent: React.FC<ListComponentProps> = ({
   const router = useRouter();
   const currentPath = router.pathname;
 
+  // State to handle the Collapse for nested links
+  const [nestedOpen, setNestedOpen] = React.useState<Record<string, boolean>>(
+    {}
+  );
+
+  const handleNestedClick = (path: string) => {
+    setNestedOpen((prev) => ({
+      ...prev,
+      [path]: !prev[path],
+    }));
+  };
+
   return (
-    <List>
-      {menuItems.map((item) =>
-        item.roles.includes(userRole) ? (
-          <ListItem
-            key={item?.path}
-            disablePadding
-            sx={{
-              display: "block",
-              color: activePath(currentPath, item?.path),
-              "&:hover": {
-                color: COLORS.primary,
-              },
-            }}
-            onClick={() => router.push(item?.path)}
-          >
-            <ListItemButton
-              sx={{
-                minHeight: 48,
-                justifyContent: open ? "initial" : "center",
-                px: 2.5,
-                onHover: {
-                  backgroundColor: "rgba(0, 0, 0, 0.04)",
-                },
-              }}
-            >
-              <ListItemIcon
+    <List
+      sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+      component="nav"
+    >
+      {menuItems.map(
+        ({ path, roles, title, icon, nestedLinks, nestedLinksItems }) =>
+          roles.includes(userRole) ? (
+            <React.Fragment key={path}>
+              <ListItem
+                disablePadding
                 sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : "auto",
-                  justifyContent: "center",
-                  color: activePath(currentPath, item?.path),
+                  display: "block",
+                  color: activePath(currentPath, path),
+                  "&:hover": {
+                    color: COLORS.primary,
+                  },
+                }}
+                onClick={() => {
+                  handleNestedClick(path);
+
+                  router.push(path);
                 }}
               >
-                <item.icon />
-              </ListItemIcon>
-              <ListItemText
-                primary={item.title}
-                sx={{ opacity: open ? 1 : 0 }}
-              />
-            </ListItemButton>
-          </ListItem>
-        ) : null
+                <ListItemButton
+                  sx={{
+                    minHeight: 48,
+                    justifyContent: open ? "initial" : "center",
+                    px: 2.5,
+                    onHover: {
+                      backgroundColor: "rgba(0, 0, 0, 0.04)",
+                    },
+                  }}
+                >
+                  <ListItemIcon
+                    sx={{
+                      minWidth: 0,
+                      mr: open ? 3 : "auto",
+                      justifyContent: "center",
+                      color: activePath(currentPath, path),
+                    }}
+                  >
+                    {icon && React.createElement(icon)} {/* Corrected here */}
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={title}
+                    sx={{ opacity: open ? 1 : 0 }}
+                  />
+                  {open &&
+                    nestedLinks &&
+                    (nestedOpen[path] ? <ExpandLess /> : <ExpandMore />)}
+                </ListItemButton>
+              </ListItem>
+              {nestedLinks && (
+                <Collapse
+                  in={nestedOpen[path] && open}
+                  timeout="auto"
+                  unmountOnExit
+                >
+                  <List component="div" disablePadding>
+                    {nestedLinksItems?.map(
+                      ({
+                        path: nestedPath,
+                        title: nestedTitle,
+                        icon: nestedIcon,
+                      }) => (
+                        <ListItem
+                          key={nestedPath}
+                          disablePadding
+                          sx={{
+                            color: activePath(currentPath, nestedPath),
+                            "&:hover": {
+                              color: COLORS.primary,
+                            },
+                          }}
+                          onClick={() => router.push(nestedPath)}
+                        >
+                          <ListItemButton
+                            sx={{
+                              minHeight: 48,
+                              justifyContent: open ? "initial" : "center",
+                              px: 2.5,
+                              onHover: {
+                                backgroundColor: "rgba(0, 0, 0, 0.04)",
+                              },
+                              pl: 4,
+                            }}
+                          >
+                            <ListItemIcon
+                              sx={{
+                                minWidth: 0,
+                                mr: open ? 3 : "auto",
+                                justifyContent: "center",
+                                color: activePath(currentPath, nestedPath),
+                              }}
+                            >
+                              {nestedIcon && React.createElement(nestedIcon)}{" "}
+                            </ListItemIcon>
+                            <ListItemText
+                              primary={nestedTitle}
+                              sx={{ opacity: open ? 1 : 0 }}
+                            />
+                          </ListItemButton>
+                        </ListItem>
+                      )
+                    )}
+                  </List>
+                </Collapse>
+              )}
+            </React.Fragment>
+          ) : null
       )}
     </List>
   );
