254913b7a9892876aa3115088e3ff8bd6deb0425
hamabarznji <hama.aak1@gmail.com>
2023-07-23 22:31:25 +0300

    added item categories table

---
+++ src/components/items/category/categoriesTable.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Table from '../../customComponents/Table';
+import ItemCategoryService from '../../../../services/ItemCategoryService';
+import { useQuery, QueryKey } from "@tanstack/react-query";
+const queryKey: QueryKey = ["items"];
+  const columns = [
+    { id: 'index', label: '#', align: 'center' },
+    { id: 'name', label: 'Category Name', align: 'center' },
+  
+  ];
+
+const InvoiceTable: React.FC = () => {
+
+  const fetchItems = async () => {
+    try {
+      const response = await ItemCategoryService.getItemCategories();
+      return response
+    } catch (error) {
+      throw new Error("Failed to fetch users");
+    }
+  };
+  const { data, refetch } = useQuery(queryKey, fetchItems);
+const rows=data?.map((item:any,index:any)=>{
+  return {
+    ...item,
+    index:index+1,
+    name:item.name,
+    
+
+  }
+})
+  return (
+    <>
+      <Table 
+        rows={rows||[]}
+        columns={columns}
+      
+      />
+    </>
+  );
+}
+
+export default InvoiceTable;
