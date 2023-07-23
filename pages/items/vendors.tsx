93ed2e45b692a481a32a2f010618702af3fe606c
hamabarznji <hama.aak1@gmail.com>
2023-07-23 23:39:55 +0300

    updated vendor page

--- pages/items/vendors.tsx
+++ pages/items/vendors.tsx
@@ -6,7 +6,6 @@ import AddVendor from "../../src/components/customComponents/InputFieldsWithVali
 import inputFields from "../../src/interfaces/user/add";
 import redirectUnauthorizedToLogin from "../../src/utils/Redirect";
 
-
 export const getServerSideProps = redirectUnauthorizedToLogin;
 type ToggleThemeFunction = () => void;
 
@@ -17,22 +16,8 @@ interface HomePageProps {
 
 const Products = ({ toggleTheme, user }: HomePageProps) => {
   return (
-    <Grid container justifyContent="flex-start" spacing={2} direction="row">
-      <Grid item container justifyContent="flex-start">
-      <AddVendor 
-      inputFields={inputFields}
-      
-      processTitle="Add Product"
-      modalTitle="Add Product"
-      submitHandler={(data: any) => console.log(data)}
-      modalType={true}
-      />
-      </Grid>
+    <ItemCategoriesTable />
 
-      <Grid item container direction="row" justifyContent="center">
-        <ItemCategoriesTable />
-      </Grid>
-    </Grid>
   );
 };
 
