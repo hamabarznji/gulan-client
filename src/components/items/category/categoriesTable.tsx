bfbb4613e288a508dc72de08c6668f4dfbd8fa89
hamabarznji <hama.aak1@gmail.com>
2023-07-23 23:40:15 +0300

    update categories table

--- src/components/items/category/categoriesTable.tsx
+++ src/components/items/category/categoriesTable.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import Table from '../../customComponents/Table';
 import ItemCategoryService from '../../../../services/ItemCategoryService';
 import { useQuery, QueryKey } from "@tanstack/react-query";
+import { Grid } from '@mui/material';
+import AddCategory from './AddCategory';
+import UpdateCategory from './UpdateCategory';
 const queryKey: QueryKey = ["items"];
   const columns = [
     { id: 'index', label: '#', align: 'center' },
     { id: 'name', label: 'Category Name', align: 'center' },
-  
+    { id: "actions", label: "Actions", align: "center" },
+
   ];
 
 const InvoiceTable: React.FC = () => {
@@ -25,17 +29,32 @@ const rows=data?.map((item:any,index:any)=>{
     ...item,
     index:index+1,
     name:item.name,
+    actions: <UpdateCategory category={item} reFetchCategories={refetch} />,
     
 
   }
 })
   return (
     <>
+      
+
+
+<Grid container justifyContent="flex-start" spacing={2} direction="row">
+      <Grid item container justifyContent="flex-start">
+      <AddCategory
+      reFetchCategories={refetch}
+       
+    
+      />
+      </Grid>
+
+      <Grid item container direction="row" justifyContent="center">
       <Table 
         rows={rows||[]}
         columns={columns}
       
-      />
+      />      </Grid>
+    </Grid>
     </>
   );
 }
