91bb7d19e0822300aa1d55b72def92ca4d623242
hamabarznji <hama.aak1@gmail.com>
2023-07-23 22:30:44 +0300

    added category page

---
+++ pages/items/category.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Grid } from "@mui/material";
+import ItemCategoriesTable from "../../src/components/items/category/categoriesTable";
+import AddProducts from "../../src/components/customComponents/InputFieldsWithValidation";
+
+import inputFields from "../../src/interfaces/user/add";
+import redirectUnauthorizedToLogin from "../../src/utils/Redirect";
+
+
+export const getServerSideProps = redirectUnauthorizedToLogin;
+type ToggleThemeFunction = () => void;
+
+interface HomePageProps {
+  toggleTheme: ToggleThemeFunction;
+  user: string;
+}
+
+const Products = ({ toggleTheme, user }: HomePageProps) => {
+  return (
+    <Grid container justifyContent="flex-start" spacing={2} direction="row">
+      <Grid item container justifyContent="flex-start">
+      <AddProducts 
+      inputFields={inputFields}
+      
+      processTitle="Add Product"
+      modalTitle="Add Product"
+      submitHandler={(data: any) => console.log(data)}
+      modalType={true}
+      />
+      </Grid>
+
+      <Grid item container direction="row" justifyContent="center">
+        <ItemCategoriesTable />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Products;
