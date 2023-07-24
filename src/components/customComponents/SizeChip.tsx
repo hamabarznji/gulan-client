e425c252a5afb64dd5c69f4c256fb828e66011b9
hamabarznji <hama.aak1@gmail.com>
2023-07-24 17:08:33 +0300

    added SizeChip for item sizes

---
+++ src/components/customComponents/SizeChip.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import Chip from "@mui/material/Chip";
+
+interface ChipsProps {
+  label: string;
+}
+
+export default function Chips({ label }: ChipsProps) {
+  let color = "default";
+  const colorMap = new Map([
+    ["small", "default"],
+    ["medium", "secondary"],
+    ["large", "error"],
+    ["Xl", "warning"],
+  ]);
+
+  color = colorMap.get(label.toString().toLowerCase()) || "warning";
+
+  if (label.toString().toLowerCase() !== "xl") {
+    label = label.toString().toUpperCase().charAt(0);
+  } else {
+    label = label.toString().toUpperCase();
+  }
+
+  return <Chip color={color} label={label} />;
+}
