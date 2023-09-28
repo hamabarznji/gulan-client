4acbe667850dd89cc1f0bd4d7c667ea5264a9808
hamabarznji <hama.aak1@gmail.com>
2023-09-29 00:37:40 +0300

    added barcode scanner

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,8 +1,11 @@
-import { Button } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Button, TextField } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import Link from "next/link";
 import { withSessionSsr } from "../utils/withSession";
 import { NextApiRequest, NextApiResponse } from "next";
+import Barcode from "react-barcode";
 
 type ToggleThemeFunction = () => void;
 
@@ -12,10 +15,46 @@ interface HomePageProps {
 }
 
 const HomePage = ({ toggleTheme, user }: HomePageProps) => {
-  const theme = useTheme();
+  const [scannedValues, setScannedValues] = useState<string[]>([]);
+  const [id, setId] = useState<string>("");
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const scannedValue = event.key;
+
+      if (scannedValue !== "Enter") {
+        setScannedValues((prevValues) => [...prevValues, scannedValue]);
+        return;
+      }
+
+      const concatedChars = scannedValues.join("");
+      const splitChars = concatedChars.split(" ");
+      const dateRegex = /\d{4}\/\d{2}\/\d{2}/;
+      const dateMatch = dateRegex.exec(concatedChars);
+
+      if (!dateMatch) {
+        console.log("Invalid barcode format");
+        setScannedValues([]);
+        setId("");
+        return;
+      }
+
+      const lastElementIsTheID = splitChars[splitChars.length - 1];
+
+      setId(lastElementIsTheID);
+      setScannedValues([]);
+    };
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [scannedValues, id]);
+
   return (
     <div>
-      
+      <h1>Barcode Generator</h1>
+      {id}
     </div>
   );
 };
