28efa93f0c9851af63909da485474e40de4a0e8b
hamabarznji <hama.aak1@gmail.com>
2023-09-29 20:31:59 +0300

    udpate data

--- src/components/invoice/add/invoiceTable/TableHead.tsx
+++ src/components/invoice/add/invoiceTable/TableHead.tsx
@@ -3,17 +3,11 @@ import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import COLORS from "../../../../../public/COLORS";
+import {columns } from './invoiceInfo'
 
-const columns = [
-    { id: "#", label: "#", align: "center" },
-    { id: "product", label: "Product", align: "center" },
-    { id: "qty", label: "QTy", align: "center" },
-    { id: "price", label: "Price", align: "center" },
-    { id: "total", label: "Total", align: "center" },
-    { id: "actions", label: "Action", align: "center" },
-  ];
 
 const CustomTableHead: React.FC = () => {
+  
     return <> <TableHead
     sx={{
       backgroundColor: COLORS.secondary,
@@ -23,7 +17,7 @@ const CustomTableHead: React.FC = () => {
       {columns.map((column) => (
         <TableCell
           key={column.id}
-          align={column?.align}
+          align={"center"}
           sx={{
             fontSize: "1rem",
             padding: "8px",
