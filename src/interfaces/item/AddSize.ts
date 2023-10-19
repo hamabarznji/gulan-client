5e7a79e25e51741a5bbf7f13ccbaf367004eddba
hamabarznji <hama.aak1@gmail.com>
2023-10-20 00:23:58 +0300

    added size input

---
+++ src/interfaces/item/AddSize.ts
@@ -0,0 +1,20 @@
+import InputsFieldInterface from "../InputsFieldInterface";
+
+const addSizeInputs: InputsFieldInterface[] = [
+  {
+    id: 1,
+    label: "Size",
+    name: "size",
+    type: "string",
+    required: true,
+    helperText: "Size is required",
+    isMenu: false,
+    isSensitive: false,
+  },
+
+ 
+ 
+
+];
+
+export default addSizeInputs;
