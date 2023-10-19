8ca1d2d039b2354784b5e7a078c683a28d9c733f
hamabarznji <hama.aak1@gmail.com>
2023-10-19 23:55:08 +0300

    fix name

--- src/components/items/category/AddCategory.tsx
+++ src/components/items/category/AddCategory.tsx
@@ -4,10 +4,10 @@ import { useSnackbar } from "notistack";
 import InputFields from "../../customComponents/InputFieldsWithValidation";
 import { QueryObserverResult } from "react-query";
 
-type AddUserProps = {
+type AddCategoryProps = {
   reFetchCategories: () => Promise<QueryObserverResult<any, unknown>>;
 };
-const AddUser: React.FC<any> = ({ reFetchCategories }: AddUserProps) => {
+const AddUser: React.FC<any> = ({ reFetchCategories }: AddCategoryProps) => {
   const { enqueueSnackbar } = useSnackbar();
 
   const submitHandler = async (data: any) => {
