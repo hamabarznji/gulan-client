bb4256278ec8529385b0eaf33afe8df1fc09e01b
hamabarznji <hama.aak1@gmail.com>
2023-10-19 23:54:06 +0300

    added color input

---
+++ src/interfaces/item/AddColor.ts
@@ -0,0 +1,20 @@
+import InputsFieldInterface from "../InputsFieldInterface";
+
+const addColorInputs: InputsFieldInterface[] = [
+  {
+    id: 1,
+    label: "Color",
+    name: "color",
+    type: "string",
+    required: true,
+    helperText: "Color is required",
+    isMenu: false,
+    isSensitive: false,
+  },
+
+ 
+ 
+
+];
+
+export default addColorInputs;
