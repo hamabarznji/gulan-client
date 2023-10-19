63cf2efd36fac4d605837531687991de871ab28f
hamabarznji <hama.aak1@gmail.com>
2023-10-20 00:25:39 +0300

    added size page

---
+++ pages/items/sizes/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Grid } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import Table from "../../../src/components/items/size/SizesTable";
+
+const Color = () => {
+  const theme = useTheme();
+  return (
+    <Grid container justifyContent="flex-start" spacing={2} direction="row">
+    
+      <Grid
+        item
+        container
+        direction="row"
+        spacing={theme.spacing(2)}
+        justifyContent="center"
+      >
+    
+      </Grid>
+      <Grid item container direction="row" justifyContent="center">
+        <Table />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Color;
