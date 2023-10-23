d262f6d259c7bac81c616d85b089099239c1f8ba
hamabarznji <hama.aak1@gmail.com>
2023-10-24 00:03:26 +0300

    seperated columns

---
+++ src/components/items/purchasedOrders/Table/columns.ts
@@ -0,0 +1,43 @@
+
+const columns = [
+    {
+      id: 10,
+      label: "#",
+      name: "index",
+      align: "center",
+      minWidth: 300,
+    },
+  
+    {
+      id: 1,
+      label: "Item Name",
+      name: "name",
+      align: "center",
+      minWidth: 300,
+    },
+    {
+      id: 2,
+      label: "Price",
+      name: "price",
+      align: "center",
+      minWidth: 170,
+    },
+    {
+      id: 3,
+      label: "Qty",
+      name: "qty",
+      align: "center",
+      minWidth: 170,
+    },
+    {
+      id: 4,
+      label: "Action",
+      name: "actions",
+      align: "center",
+      minWidth: 170,
+    },
+  ];
+
+  
+
+  export default columns;
