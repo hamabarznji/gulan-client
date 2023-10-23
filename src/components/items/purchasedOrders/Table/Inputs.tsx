69db28a0d676da20102fb6e52e0fa9fc7f34c0fb
hamabarznji <hama.aak1@gmail.com>
2023-10-24 00:03:58 +0300

    changed the coponent strcutre

--- src/components/items/purchasedOrders/Table/Inputs.tsx
+++ src/components/items/purchasedOrders/Table/Inputs.tsx
@@ -1,108 +1,56 @@
-import React from "react";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Controller, useForm } from "react-hook-form";
-import { TableCell, Grid, Button } from "@mui/material";
-import generateSchema from "../../../../utils/SchemaGenerator";
+import React, { useState } from "react";
+import { Controller } from "react-hook-form";
+import { Grid } from "@mui/material";
 import TextField from "../../../customComponents/TextField";
-import inputFields from "../../../../interfaces/item/AddPurchaseItem";
-import DropDownMenu from "../../../customComponents/DropDownMenu";
-import Butoon from "../../../customComponents/Button";
-import ItemsServiceInstance from "../../../../../services/ItemService";
-import { useQuery, QueryKey } from "@tanstack/react-query";
-
 
 interface InputFieldsWithValidationProps {
-  submitHandler?: (data: any) => void;
-  modalType?: boolean;
-  children?: React.ReactNode;
-  onAddItem?: (data:any) => void;
+  field: any;
+  control: any;
+  defaultValue?: any;
+  disabled?: boolean;
+  hidden?: boolean;
+  errors: any;
 }
 
-// ... (other imports)
-
 const Inputs: React.FC<InputFieldsWithValidationProps> = ({
-  onAddItem,
+  field,
+  control,
+  defaultValue="",
+  disabled,
+  hidden=false,
+  errors,
 }) => {
-  const yupSchema = generateSchema(inputFields);
-  const {
-    control,
-    formState: { errors },
-    reset,
-    handleSubmit,
-  } = useForm({
-    resolver: yupResolver(yupSchema),
-  });
-
-  const onSubmit = (data: any) => {
-    console.log({data})
-    onAddItem(data);
-    reset({
-      id: "",
-      qty: "",
-      price: "",
-    }); 
-  };
-  const fetchItems = async () => {
-    try {
-      const response = await ItemsServiceInstance.getItemsForPurchaseInvoice();
-      return response
-    } catch (error) {
-      throw new Error("Failed to fetch users");
-    }
-  };
-
-  const { data, refetch } = useQuery(["getItems"], fetchItems);
-  const updatedInputs = inputFields.map((input) => {
-    if (input.name === "id") {
-      input.options = data || [];
-    }
-    return input;
-  });
-
+ 
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Grid container spacing={2} direction={"row"}>
-        {updatedInputs?.map((field, index) => (
-          <Grid item xs={3} key={field.name}>
-            <Controller
-              name={field.name}
-              control={control}
-              render={({ field: inputField }) => {
-                let CommonProps = {
-                  ...inputField,
-                  label: field.label,
-                  error: !!errors[field.name],
-                  helperText: errors[field.name]?.message?.toString(),
-                  ref: null,
-                  defaultValue: !field.isSensitive ? inputField.value : "",
-                };
+    <Grid item xs={12} key={field.name}>
+      <Controller
+        name={field.name}
+        control={control}
+        render={({ field: inputField }) => {
+          let CommonProps = {
+            ...inputField,
+            label: field.label,
+            error: !!errors[field.name],
+            helperText: errors[field.name]?.message?.toString(),
+            ref: null,
+            defaultValue: !field.isSensitive ? inputField.value : "",
+          };
 
-                return field.isMenu ? (
-                  <DropDownMenu
-                    options={field?.options || []}
-                    {...CommonProps}  
-                    isValueAndName={true}
-                 
-                 
-                  />
-                ) : (
-                  <TextField {...CommonProps} type={field.type} isDate={false} />
-                );
-              }}
+          return (
+            <TextField
+              {...CommonProps}
+              type={field.type}
+              fullWidth
+              defaultValue={defaultValue }
+              disabled={disabled}
+              hidden={hidden}
             />
-          </Grid>
-        ))}
-        <Grid item xs={3}>
-          <Butoon title="Add" type="submit">
-            Add
-          </Butoon>
-        </Grid>
-      </Grid>
-    </form>
+          );
+        }}
+      />
+    </Grid>
   );
 };
 
 export default Inputs;
-
-
