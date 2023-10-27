eef42d42965bb51a27a6b41812d93301da0890b4
hamabarznji <hama.aak1@gmail.com>
2023-10-27 19:12:15 +0300

    added real data

--- src/components/invoice/InvoicesTable.tsx
+++ src/components/invoice/InvoicesTable.tsx
@@ -1,27 +1,49 @@
 import React from "react";
 import Table from "../../components/customComponents/Table";
+import SellOrderServiceInstance from "../../../services/SellOrderService";
+import { useQuery, QueryKey } from "@tanstack/react-query";
+import CustomButton from "../customComponents/Button";
+import { Router, useRouter } from "next/router";
 
-const rows = Array.from(Array(50), (_, index) => ({
-  id: index + 1,
-  name: `Person ${index + 1}`,
-  age: Math.floor(Math.random() * 50) + 20,
-  email: `person${index + 1}@example.com`,
-}));
+const SELLORDERS: QueryKey = ["expenseCategories"];
 
 const columns = [
-  { id: "id", label: "ID", align: "center" },
-  { id: "name", label: "Name", align: "center" },
-  { id: "age", label: "Age", align: "center" },
-  { id: "email", label: "Email", align: "center" },
+  { id: "index", label: "#", align: "center" },
+  { id: "createdBy", label: "Created By", align: "center" },
+  { id: "createdAt", label: "Created At", align: "center" },
+  { id: "updatedAt", label: "Updated At", align: "center" },
   { id: "actions", label: "Action", align: "center" },
 ];
 
 const InvoiceTable: React.FC = () => {
-  // Your component logic here
+  const router = useRouter();
+
+  const getSellOrders = async () => {
+    try {
+      return SellOrderServiceInstance.getSellOrders();
+    } catch (err: any) {
+      console.log(err);
+      throw new Error(err);
+    }
+  };
+
+  const { data } = useQuery(SELLORDERS, getSellOrders);
+
+  const rows = data?.map((row: any, index: number) => ({
+    ...row,
+    actions: (
+      <CustomButton
+        title="View"
+        onClick={() => {
+          router.push(`/invoices/${row.id}`);
+        }}
+      />
+    ),
+  }));
 
   return (
     <>
-      <Table rows={rows} columns={columns} />
+      <Table rows={rows || []} columns={columns} />
     </>
   );
 };
