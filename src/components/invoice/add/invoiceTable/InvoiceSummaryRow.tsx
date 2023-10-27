0e2a9add2ccc14d670bb754da0c2ef827a1d29e5
hamabarznji <hama.aak1@gmail.com>
2023-10-27 17:46:38 +0300

    added submit handler

--- src/components/invoice/add/invoiceTable/InvoiceSummaryRow.tsx
+++ src/components/invoice/add/invoiceTable/InvoiceSummaryRow.tsx
@@ -3,6 +3,13 @@ import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import COLORS from "../../../../../public/COLORS";
 import Textfield from "../../../customComponents/TextField";
+import Modal from "../../../customComponents/Modal";
+import { Typography } from "@mui/material";
+import { useRouter } from "next/router";
+import { useSnackbar } from "notistack";
+import useUserContext from "../../../../context/useUserContext";
+import SellOrderServiceInstance from "../../../../../services/SellOrderService";
+
 interface RowProps {
   items: Row[];
 }
@@ -17,7 +24,43 @@ interface Row {
 }
 
 const CustomTableRow: React.FC<RowProps> = ({ items }) => {
-  const [discount,setDiscount]=useState(0)
+  const [discount, setDiscount] = useState(0);
+  const router = useRouter();
+  const { enqueueSnackbar } = useSnackbar();
+  const user = useUserContext().user;
+
+  const submitHandler = async () => {
+    try {
+      const orderInfo = {
+        user_id: user!!.id,
+        discount: discount,
+      };
+      const data = items.map((item) => {
+        return {
+          item_id: item.id,
+          qty: item.qty,
+          price: item.price,
+        };
+      });
+      const order = {
+        orderInfo,
+        items: data,
+      };
+      const response = await SellOrderServiceInstance.createSellOrders(order);
+      if (response === 200) {
+        enqueueSnackbar("New Sell Order Created Successfully!", {
+          variant: "success",
+        });
+        router.push("/invoices");
+      } else {
+        throw new Error("Failed to create new sell order");
+      }
+    } catch (error) {
+      enqueueSnackbar("Error: " + error.message, {
+        variant: "error",
+      });
+    }
+  };
   return (
     <>
       <TableRow>
@@ -50,11 +93,10 @@ const CustomTableRow: React.FC<RowProps> = ({ items }) => {
             fontWeight: "bold",
           }}
         >
-<Textfield
-onChange={(e)=>setDiscount(e?.target?.value)}
-defaultValue={0}
-
-/>
+          <Textfield
+            onChange={(e: any) => setDiscount(e?.target?.value)}
+            defaultValue={0}
+          />
         </TableCell>
       </TableRow>
       <TableRow>
@@ -87,7 +129,7 @@ defaultValue={0}
             fontWeight: "bold",
           }}
         >
-          {items.reduce((sum, i) => sum +  i.total, 0)}
+          {items.reduce((sum, i) => sum + i.total, 0)}
         </TableCell>
       </TableRow>
 
@@ -112,8 +154,36 @@ defaultValue={0}
           }}
           align="right"
         >
-          {(items.reduce((sum, i) => sum + i.total , 0) -
-            items.reduce((sum, i) => sum + i.total  , 0) * (discount/100)).toFixed(2)}
+          {(
+            items.reduce((sum, i) => sum + i.total, 0) -
+            items.reduce((sum, i) => sum + i.total, 0) * (discount / 100)
+          ).toFixed(2)}
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell
+          colSpan={10}
+          sx={{
+            fontSize: "1rem",
+            padding: "8px",
+            color: COLORS.secondary,
+            fontWeight: "bold",
+          }}
+        >
+          <Modal
+            processTitle={"Add New Invoice"}
+            modalTitle={"Add New Invoice"}
+            submitHandler={submitHandler}
+            customeStyles={styles}
+          >
+            <Typography
+              variant="h5"
+              style={{ textAlign: "center", color: "black" }}
+            >
+              Proceed with invoice creation by clicking the button. Are you sure
+              you want to continue?
+            </Typography>
+          </Modal>
         </TableCell>
       </TableRow>
     </>
@@ -121,3 +191,11 @@ defaultValue={0}
 };
 
 export default CustomTableRow;
+
+const styles = {
+  contentStyle: {
+    minWidth: "200px",
+    minHeight: "100px",
+    marginTop: "1rem",
+  },
+};
