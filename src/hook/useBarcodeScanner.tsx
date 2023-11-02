da57a6016283b958e079f7f4af8c4eecb6023c3c
hamabarznji <hama.aak1@gmail.com>
2023-11-02 22:17:20 +0300

    refactored the code and added isLoading varibale

--- src/hook/useBarcodeScanner.tsx
+++ src/hook/useBarcodeScanner.tsx
@@ -1,59 +1,56 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import ItemsServiceInstance from "../../services/ItemService";
-import { useSnackbar } from "notistack";
 
-const useBarcodeScanner = () => {
-  const [id, setId] = useState("");
-  const [item, setItem] = useState();
+const useScanner = () => {
+  const [scannedValues, setScannedValues] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [scannedItem, setScannedItem] = useState(null);
 
-  const [scannedValues, setScannedValues] = useState<string[]>([]);
-  const { enqueueSnackbar } = useSnackbar();
+  const handleKeyDown = useCallback(
+    async (event: any) => {
+      setIsLoading(true);
+      try {
+        const scannedValue = event.key;
+
+        if (scannedValue !== "Enter") {
+          setScannedValues((prevValues) => [...prevValues, scannedValue]);
+          return;
+        }
+        setIsLoading(true);
+
+        const concatedChars = scannedValues.join("");
+        const splitChars = concatedChars.split(" ");
+        const dateRegex = /\d{4}\/\d{2}\/\d{2}/;
+        const dateMatch = dateRegex.exec(concatedChars);
+
+        if (!dateMatch) {
+          console.error("Invalid barcode format");
+          setScannedValues([]);
+          return;
+        }
+
+        const lastElementIsTheID = splitChars[splitChars.length - 1];
+
+        setScannedValues([]);
+        const res = await ItemsServiceInstance.getItemById(lastElementIsTheID);
+        setScannedItem(res);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error fetching item:", error);
+      }
+    },
+    [scannedValues]
+  );
 
   useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      const scannedValue = event.key;
-
-      if (scannedValue !== "Enter") {
-        setScannedValues((prevValues) => [...prevValues, scannedValue]);
-        return;
-      }
-
-      const concatedChars = scannedValues.join("");
-      const splitChars = concatedChars.split(" ");
-      const dateRegex = /\d{4}\/\d{2}\/\d{2}/;
-      const dateMatch = dateRegex.exec(concatedChars);
-
-      if (!dateMatch) {
-        // Add more structured error handling here
-        console.error("Invalid barcode format");
-        setScannedValues([]);
-        setId("");
-        return;
-      }
-
-      const lastElementIsTheID = splitChars[splitChars.length - 1];
-
-      setId(lastElementIsTheID);
-      setScannedValues([]);
-      ItemsServiceInstance.getItemById(lastElementIsTheID)
-        .then((res) => {
-          setItem(res);
-        })
-        .catch((error) => {
-          enqueueSnackbar("Error: " + error.message, {
-            variant: "error",
-          });
-          console.error("Error fetching item:", error);
-        });
-    };
-
     document.addEventListener("keydown", handleKeyDown);
 
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
-  });
-  return item  || null;
+  }, [handleKeyDown]);
+
+  return { scannedItem, isLoading };
 };
 
-export default useBarcodeScanner;
+export default useScanner;
