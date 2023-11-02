4747120f0f920d3976bdb19617aeabc7c60df492
hamabarznji <hama.aak1@gmail.com>
2023-11-03 00:04:45 +0300

    removed extra lines

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,5 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Button, TextField } from "@mui/material";
-import { useTheme } from "@mui/material/styles";
-import Link from "next/link";
-import { withSessionSsr } from "../utils/withSession";
-import { NextApiRequest, NextApiResponse } from "next";
-import Barcode from "react-barcode";
-import useBarcodeScanner from "../src/hook/useBarcodeScanner";
+import React from "react";
+
 type ToggleThemeFunction = () => void;
 
 interface HomePageProps {
@@ -14,41 +7,14 @@ interface HomePageProps {
   user: string;
 }
 
-const HomePage = ({ toggleTheme, user }: HomePageProps) => {
-  const id=useBarcodeScanner()
-
-  
+const Dashboard = () => {
 
   return (
     <div>
-      <h1>Barcode Generator</h1>
-      {id}
+      <h1>Dashboard</h1>
+    
     </div>
   );
 };
 
-export default HomePage;
-
-export const getServerSideProps = withSessionSsr(
-  async ({
-    req,
-    res,
-  }: {
-    req: NextApiRequest & { session: { user?: string } };
-    res: NextApiResponse;
-  }) => {
-    const user = req.session.user;
-
-    if (!user) {
-      return {
-        redirect: {
-          destination: "/login",
-          permanent: true,
-        },
-      };
-    }
-    return {
-      props: { user },
-    };
-  }
-);
+export default Dashboard;
