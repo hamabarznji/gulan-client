5cbe026c68b0f11898b5d5d1cee3b8fcb252f8ce
hamabarznji <hama.aak1@gmail.com>
2023-11-13 14:33:25 +0300

    removed files

--- pages/500.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const InternalServerError: React.FC = () => {
-  return <h1 style={{ fontSize: '2em', textAlign: 'center' }}>500 - Internal Server Error</h1>;
-};
-
-export default InternalServerError;
