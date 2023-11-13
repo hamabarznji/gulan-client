c2ff301e35fa2ab857f9a5fd1d29ee4e5f72fd0d
hamabarznji <hama.aak1@gmail.com>
2023-11-13 12:00:24 +0300

    fixed redirect

--- src/utils/Redirect.ts
+++ src/utils/Redirect.ts
@@ -2,6 +2,16 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { withSessionSsr } from "../../utils/withSession";
 import menuItems from "../interfaces/menuItems";
 
+
+interface User {
+  role?: string;
+}
+
+interface MenuItem {
+  path: string;
+  roles: string[];
+}
+
  const isCurrentUserAllowed = withSessionSsr(
   async ({
     req,
@@ -26,7 +36,7 @@ import menuItems from "../interfaces/menuItems";
       .filter(item => item.path === currentPath)
       .map(item => item.roles)
       .flat();
-        {/* @ts-ignore */}
+      /* @ts-ignore */
     if (user?.role as string !== "admin" && !allowedRoles.includes(user?.role as string)) {
       return {
         redirect: {
