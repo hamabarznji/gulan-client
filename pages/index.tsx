d14f12efe5a4592532cec85bedc41d014708ac83
hamabarznji <hama.aak1@gmail.com>
2023-11-13 14:51:56 +0300

    removed extra files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,53 @@
-import * as React from "react";
-import UsersTable from "../src/components/user/UsersTable";
-import isCurrentUserAllowed from "../src/utils/Redirect";
+import React, { FormEvent } from "react";
+import { withSessionSsr } from "../utils/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import Card from "../src/components/login/loginForm";
+import Image from "next/image";
+import loginImage from "../public/login.svg";
 
-export const getServerSideProps = isCurrentUserAllowed;
+interface LoginPageProps {
+  user: any;
+}
 
-const Users = () => {
-  return <UsersTable />;
-};
-export default Users;
+export default function LoginPage({ user }: LoginPageProps) {
+  return (
+    <Grid container spacing={0} sx={{ height: "100vh" }}>
+      <Grid xs={12} sm={6}>
+        <Image
+          src={loginImage}
+          alt="Login"
+          style={{ maxWidth: "80%", height: "100%" }}
+        />
+      </Grid>
+
+      <Grid xs={12} sm={6}>
+        <Card />
+      </Grid>
+    </Grid>
+  );
+}
+
+export const getServerSideProps = withSessionSsr(
+  async ({
+    req,
+    res,
+  }: {
+    req: NextApiRequest & { session: { user?: string } };
+    res: NextApiResponse;
+  }) => {
+    const user = req?.session?.user || null;
+    if (user) {
+      return {
+        redirect: {
+          destination: "/",
+          permanent: true,
+        },
+      };
+    }
+
+    return {
+      props: { pageTitle: "Login", user },
+    };
+  }
+);
